d372e40198fa107a3177aa911319353893e872b1
Aman Gangurde <ios@webwingtechnologies.com>
2019-11-05 15:12:14 +0530

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Aman Gangurde on 11/5/19.
+//  Copyright © 2019 Webwing. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
